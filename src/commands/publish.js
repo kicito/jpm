9549215d6a6aca6bdb3f8dd5b926a6dfa9bf9dee
Luis Antonio Canettoli <hello@luisanton.io>
2021-03-21 17:28:31 +0100

    Publishing on NPM

--- src/commands/publish.js
+++ src/commands/publish.js
@@ -1,76 +1,23 @@
-const AdmZip = require('adm-zip');
-const fs = require("fs-extra")
-const glob = require("glob")
-const fetch = require("node-fetch");
-const FormData = require('form-data');
-
-const { JPM_ENDPOINT } = require('../constants/jpmEndpoint');
-const { JPM_CACHE } = require('../utils/jpmCache');
 const { JPM_JSON } = require("../utils/jpmJson");
-
-const chalk = require('chalk');
-
-const NOT_LOGGED_IN = `Not logged in. Login first using: ${chalk.cyanBright('jpmc login <username> <password>')}`
+const fs = require("fs-extra")
+const os = require("os");
+const { exec } = require("../utils/exec");
+const { execSync } = require("child_process");
 
 function publish() {
-    if (!JPM_JSON.exists()) throw new Error("JPM not initialized")
-    const { name: pkgName } = JPM_JSON.read()
+    const packageJson = JPM_JSON.read()
 
-    if (!JPM_CACHE.exists()) throw new Error(NOT_LOGGED_IN)
-    const { jwt } = JPM_CACHE.read()
-    if (!jwt) throw new Error(NOT_LOGGED_IN)
+    delete packageJson.dependencies
+    delete packageJson.scripts
 
-    const { token } = jwt
-    const tmp = ".__tmp"
-    const zipDest = `${tmp}/${pkgName}`
+    fs.writeFileSync('package.json', JSON.stringify(packageJson, null, "   ") + os.EOL)
+    fs.writeFileSync('.npmignore', "lib/\ntarget/\n.settings/\n.classpath\n.project\nsrc/\npom.xml\npackages/" + os.EOL)
 
-    fs.existsSync(tmp) && fs.removeSync(tmp, { recursive: true })
+    execSync("npm publish", { stdio: [0, 1, 2] })
 
-    console.log(pkgName)
-
-    fs.mkdirSync(tmp)
-    fs.mkdirSync(zipDest)
-
-    const zip = new AdmZip()
-
-    glob(`!(packages|lib|${tmp})`, (err, files) => {
-        for (let file of files) fs.copyFileSync(file, `${zipDest}/${file}`)
-        zip.addLocalFolder(tmp)
-
-        const zipName = `${pkgName}.zip`
-        zip.writeZip(zipName)
-
-        fs.removeSync(tmp, { recursive: true })
-
-        uploadArtifact(zipName, token)
-
-    })
-
+    fs.removeSync("package.json")
 
 }
 
-
-async function uploadArtifact(zipName, token) {
-    const { name: handle, version, tags } = JPM_JSON.read()
-    const body = new FormData();
-    body.append("archive", fs.createReadStream(zipName));
-    body.append("packageData", JSON.stringify({ handle, version, tags }));
-
-    const response = await fetch(`${JPM_ENDPOINT}/pkgs`, {
-        method: 'POST',
-        headers: {
-            "Authorization": `Bearer ${token}`
-        },
-        body
-    })
-
-    const result = await response.json()
-
-    console.log(chalk.cyanBright("Response status: " + response.status))
-    console.log(result)
-
-    fs.removeSync(zipName)
-
-}
 
 module.exports = { publish }
