26518793b1aa26afaa2a066e4eb7e7d4a5c92b42
Luis Antonio Canettoli <hello@luisanton.io>
2021-03-24 22:11:16 +0100

    Recursively adding artifacts with peers from MVN

--- src/commands/add.js
+++ src/commands/add.js
@@ -1,67 +1,124 @@
 const fetch = require("node-fetch");
-const extract = require('extract-zip')
-const path = require("path")
 const fs = require("fs-extra")
 
 const { JPM_JSON } = require("../utils/jpmJson")
-const { JPM_ENDPOINT } = require("../constants/jpmEndpoint")
-const { PACKAGE_DIR } = require("../constants/packageDir")
-const { REPOSITORIES } = require("../constants/repositories")
+const PACKAGE_DIR = require("../constants/packageDir")
+const REPOSITORIES = require("../constants/repositories")
+const LATEST_VERSION = require("../constants/latestVersion");
 
-const { updateJpmJson } = require("../utils/updateJpmJson")
+const { updateDependencies, updatePeers } = require("../utils/updateJpmJson")
+const { downloadMvnArtifact } = require("../utils/downloadMvnArtifact");
 const { downloadArtifact } = require("../utils/downloadArtifact");
-const { exec } = require("../utils/exec");
+const semver = require("semver");
 
-const postinstall = () => {
-    exec("jpm run postinstall")
-}
+const { makeMvnArtifactJson } = require("../utils/makeMvnArtifactJson")
 
-async function add(artifact) {
-    const [artifactName, version] = artifact.split("^")
+const tar = require("tar");
+
+async function add(artifactRef, parentArtifact) {
 
     try {
         if (!JPM_JSON.exists()) throw new Error("No jpm.json found in this folder.")
 
-        const [pkgRepo, pkgName] =
-            artifactName.slice(-4, -3) === '@'
-                ? [artifactName.slice(-3), artifactName.slice(0, -4)]
-                : ['jpm', artifactName]
+        const [repo, artifact] =
+            artifactRef.slice(-4, -3) === '@'
+                ? [artifactRef.slice(-3), artifactRef.slice(0, -4)]
+                : ['npm', artifactRef]
 
-        if (!REPOSITORIES.map(r => `@${r}`).includes(`@${pkgRepo}`)) {
-            throw new Error("Unsupported package repository: " + pkgRepo)
+        if (!REPOSITORIES.map(r => `@${r}`).includes(`@${repo}`)) {
+            throw new Error("Unsupported package repository: " + repo)
         }
 
-        console.log(pkgRepo)
+        const { installedArtifact, artifactJson } = await (() => {
+            switch (repo) {
+                case 'mvn': return addMvnArtifact(artifact)
+                case 'npm': return addNpmArtifact(artifact)
+            }
+        })()
 
-        const fileUrl = `${JPM_ENDPOINT}/pkgs/${pkgName}/${version ? version : ''}`
-        console.log(fileUrl)
-        const response = await fetch(fileUrl)
-        const package = await response.json()
+        !parentArtifact && updateDependencies(repo, installedArtifact)
+        await updatePeers(repo, parentArtifact, installedArtifact)
 
-        if (response.ok) {
-            !fs.existsSync(PACKAGE_DIR) && fs.mkdirSync(PACKAGE_DIR)
+        console.log(`Installed ${parentArtifact ? 'peer dependency' : ''}: ${installedArtifact}`)
 
-            const archivePath = await downloadArtifact(pkgName, package.version.file.secure_url)
+        await addArtifactDependencies({ repo, artifactJson })
 
-            console.log(archivePath)
-            await extract(archivePath, { dir: path.resolve(PACKAGE_DIR) })
-            await fs.removeSync(archivePath)
-            console.log('Installation complete.')
-
-            updateJpmJson({ pkgRepo, pkgName, version: package.version.semantic })
-        } else {
-            if (response.status === 404)
-                throw new Error(`Package ${pkgName} not found @ ${pkgRepo}`)
-            else
-                throw new Error("Error connecting to Jolie Package Manager.")
-        }
-
-        JPM_JSON.read().scripts.postinstall && postinstall()
-
+        console.log('Done!')
 
     } catch (error) {
         console.error(error.message)
     }
 }
+
+
+async function addArtifactDependencies({ repo, artifactJson }) {
+
+    Object.entries(artifactJson).forEach(([artifactId, version]) => {
+        add(`${artifactId}${version !== LATEST_VERSION ? `:${version}` : ''}@${repo}`, { artifactId, artifactJson })
+    })
+
+}
+
+
+async function addMvnArtifact(artifact) {
+    const [groupId, artifactId, _version] = artifact.split(":")
+
+    const version = _version || await getLatestArtifactVersion({ groupId, artifactId }) //throw new Error("Please specify a version when downloading from Maven.")
+
+    await downloadMvnArtifact({ groupId, artifactId, version })
+
+    const artifactJson = await makeMvnArtifactJson({ groupId, artifactId, version })
+
+    return { installedArtifact: `${groupId}:${artifactId}:${version}`, artifactJson }
+}
+
+async function getLatestArtifactVersion({ groupId, artifactId }) {
+    const response = await fetch(`http://search.maven.org/solrsearch/select?q=g:%22${groupId}%22+AND+a:%22${artifactId}%22`)
+
+    if (!response.ok) throw new Error("Error connecting to Maven.")
+
+    const { response: { docs } } = await response.json()
+
+    if (docs.length === 0) throw new Error("Artifact not found @ Maven.")
+    return docs[0].latestVersion
+}
+
+async function addNpmArtifact(artifact) {
+    const [artifactName, _version] = artifact.split(":")
+
+    const response = await fetch(`https://registry.npmjs.org/${artifactName}`)
+
+    if (response.status === 404)
+        throw new Error(`Package ${artifactName} not found @ npm.`)
+    else if (!response.ok)
+        throw new Error("Error connecting to npm.")
+
+    const body = await response.json()
+
+    // Use latest when not specified
+    const version = _version || Object.keys(body.versions).sort((a, b) => semver.gt(a, b) ? -1 : 1)[0]
+
+    if (!body.versions[version]) throw new Error("Specified version not found.")
+
+    const { tarball } = body.versions[version].dist
+
+    const archivePath = await downloadArtifact({ artifactName, artifactUrl: tarball })
+    const archiveDir = `${PACKAGE_DIR}/${artifactName}`
+
+    fs.mkdirSync(archiveDir, { recursive: true })
+    const stream = fs.createReadStream(archivePath).pipe(tar.x({ strip: 1, C: archiveDir, sync: true }))
+
+    await new Promise((resolve) => stream.on('end', () => resolve()))
+
+    fs.removeSync(archivePath)
+
+    updateJpmJson('npm', artifactName, version)
+
+    const artifactJson = fs.existsSync(`${archiveDir}/jpm.json`) && fs.readJsonSync(`${archiveDir}/jpm.json`)
+
+    return { installedArtifact: `${artifactName}:${version}`, artifactJson }
+}
+
+
 
 module.exports = { add }
