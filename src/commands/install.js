6feb5e4b251152039f3a0541f2b337e21f247fdc
Luis Antonio Canettoli <hello@luisanton.io>
2021-03-24 22:16:38 +0100

    Creating INSTALL command

--- src/commands/install.js
+++ src/commands/install.js
@@ -1,7 +1,21 @@
-function install() {
-    // [pkgName, version] = args[0].split('^')
-    // install({ pkgName, version })
-    console.log("TODO")
+const LATEST_VERSION = require("../constants/latestVersion")
+const REPOSITORIES = require("../constants/repositories")
+const { JPM_JSON } = require("../utils/jpmJson")
+const { add } = require("./add")
+
+async function install() {
+    const jpmJson = JPM_JSON.read()
+
+    for (let repo of REPOSITORIES) {
+        const depEntries = Object.entries(jpmJson.dependencies[repo])
+
+        for (let [artifactId, version] of depEntries) {
+            const adding = `${artifactId}${version !== LATEST_VERSION ? `:${version}` : ''}@${repo}`
+
+            console.log({ adding })
+            await add(adding)
+        }
+    }
 
 }
 
