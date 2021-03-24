72b58968148cf616aae9a1f174902948e4a25f29
Luis Antonio Canettoli <hello@luisanton.io>
2021-03-24 22:13:28 +0100

    Using parsed version in jpm.json

--- src/utils/makeMvnArtifactJson.js
+++ src/utils/makeMvnArtifactJson.js
@@ -2,6 +2,7 @@ const fs = require("fs-extra");
 const { parsePom } = require("./parsePom")
 const fetch = require("node-fetch")
 const { makeArtifactPomUrl } = require("./downloadMvnArtifact");
+const { parseMvnVersion } = require("./parseMvnVersion");
 
 async function makeMvnArtifactJson({ groupId, artifactId, version }) {
     const response = await fetch(makeArtifactPomUrl({ groupId, artifactId, version }))
@@ -10,7 +11,10 @@ async function makeMvnArtifactJson({ groupId, artifactId, version }) {
     await fs.writeFile(pomPath, await response.text())
 
     const parsedPom = await parsePom({ filePath: pomPath })
-    console.log({ parsedPom })
+
+    Object.keys(parsedPom).forEach(
+        key => (parsedPom[key] = parseMvnVersion(parsedPom[key]))
+    )
 
     fs.remove(pomPath)
 
