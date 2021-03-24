4497d34ae143d4c8b8b09cdf5cd0b70f8903e36a
Luis Antonio Canettoli <hello@luisanton.io>
2021-03-24 22:03:29 +0100

    Checking JPM_JSON existing on .read

--- src/utils/jpmJson.js
+++ src/utils/jpmJson.js
@@ -1,10 +1,15 @@
 const fs = require("fs-extra");
 const os = require("os");
 
+const jpmJsonPath = './jpm.json'
+
 const JPM_JSON = {
-    exists: () => fs.existsSync('./jpm.json'),
-    read: () => fs.readJsonSync('./jpm.json'),
-    write: (jpmJson) => fs.writeFileSync('./jpm.json', JSON.stringify(jpmJson, null, "   ") + os.EOL)
+    exists: () => fs.existsSync(jpmJsonPath),
+    read: function () {
+        if (!this.exists()) throw new Error("No jpm.json found in this folder.")
+        return fs.readJsonSync(jpmJsonPath)
+    },
+    write: (jpmJson) => fs.writeFileSync(jpmJsonPath, JSON.stringify(jpmJson, null, "   ") + os.EOL)
 }
 
 module.exports = { JPM_JSON }
