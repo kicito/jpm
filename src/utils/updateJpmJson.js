fb8af7721feeaf043b7327f05cba05dc49ea56db
Luis Antonio Canettoli <hello@luisanton.io>
2021-03-24 22:15:51 +0100

    Writing correct peer deps from mvn

--- src/utils/updateJpmJson.js
+++ src/utils/updateJpmJson.js
@@ -1,29 +1,62 @@
+const { makeArtifactPomUrl } = require("./downloadMvnArtifact")
 const { JPM_JSON } = require("./jpmJson")
 
-function updateJpmJson({ pkgRepo, pkgName, version }) {
-    let jpmJson = JPM_JSON.read()
+const fs = require("fs-extra")
+const fetch = require("node-fetch")
+const { parsePom } = require("./parsePom")
+const { parseMvnVersion } = require("./parseMvnVersion")
+const { makeMvnArtifactJson } = require("./makeMvnArtifactJson")
 
-    jpmJson = {
-        ...jpmJson,
-        dependencies: {
-            ...jpmJson.dependencies,
-            [pkgRepo]: (() => {
-                switch (pkgRepo) {
-                    // case 'mvn': 
-                    //     const [groupId, artifactId, version] = pkgName.split(':')
-                    //     return { groupId, artifactId, version }
-                    case 'jpm':
-                        return {
-                            ...jpmJson.dependencies.jpm,
-                            [pkgName]: version
-                        }
+function updateDependencies(repo, artifact) {
 
-                }
-            })()
-        }
+    const [version, artifactName, groupId] = artifact.split(":").reverse()
+
+    const artifactId = groupId ? `${groupId}:${artifactName}` : artifactName
+    const jpmJson = JPM_JSON.read()
+
+    jpmJson.dependencies[repo] = {
+        ...jpmJson.dependencies[repo],
+        [artifactId]: version
     }
 
     JPM_JSON.write(jpmJson)
 }
 
-module.exports = { updateJpmJson }
+async function updatePeers(repo, installedArtifact) {
+
+    const jpmJson = JPM_JSON.read()
+
+    if (!jpmJson.peerDependencies) jpmJson.peerDependencies = {}
+
+    let artifactJson = {}
+
+    switch (repo) {
+        case 'mvn':
+            if (!jpmJson.peerDependencies.mvn)
+                jpmJson.peerDependencies.mvn = {}
+
+            const [groupId, artifactId, version] = installedArtifact.split(":")
+            const parsedPom = await makeMvnArtifactJson({ groupId, artifactId, version })
+
+            console.log({ parsedPom })
+
+            Object.keys(parsedPom).length > 0 && (
+                jpmJson.peerDependencies.mvn[`${groupId}:${artifactId}`] = parsedPom
+            )
+
+
+            artifactJson = parsedPom
+
+            break
+        case 'npm':
+            //TODO
+            artifactJson = {}
+    }
+
+    JPM_JSON.write(jpmJson)
+
+    return artifactJson
+
+}
+
+module.exports = { updateDependencies, updatePeers }
