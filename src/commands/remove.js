31fb435112d0ced57e328bdcc28c9fe74014971c
Luis Antonio Canettoli <hello@luisanton.io>
2021-03-28 19:45:10 +0200

    Added REMOVE command.

--- src/commands/remove.js
+++ src/commands/remove.js
@@ -1,18 +1,40 @@
+const fs = require("fs-extra");
 const PACKAGE_DIR = require('../constants/packageDir');
+const REPOSITORIES = require("../constants/repositories");
+const LIB_DIR = require('../constants/lib');
 const { JPM_JSON } = require('../utils/jpmJson');
-const fs = require("fs-extra")
+const { install } = require("./install");
+const chalk = require('chalk')
 
-function remove(artifact) {
-
-    const [artifactName] = artifact.split("^")
-
-    fs.removeSync(`${PACKAGE_DIR}/${artifactName}`, { recursive: true });
+function remove(artifactRef) {
 
     const jpmJson = JPM_JSON.read()
 
-    delete jpmJson.dependencies.jpm[artifactName]
+    const [repo, artifact] =
+        artifactRef.slice(-4, -3) === '@'
+            ? [artifactRef.slice(-3), artifactRef.slice(0, -4)]
+            : ['npm', artifactRef]
+
+    if (!REPOSITORIES.map(r => `@${r}`).includes(`@${repo}`)) {
+        throw new Error("Package repository " + repo + " does not exist.")
+    }
+
+    const split = artifact.split(":")
+
+    if (split.length > 2) {
+        throw new Error(`Usage: ${chalk.green("jpm remove pkg-name@repo")}`)
+    }
+
+    delete jpmJson.dependencies[repo][artifact]
+    delete jpmJson.mvnPeers
+
+    fs.removeSync(`${PACKAGE_DIR}`, { recursive: true });
+    fs.removeSync(`${LIB_DIR}`, { recursive: true });
 
     JPM_JSON.write(jpmJson)
+
+    install()
+
 }
 
 module.exports = { remove }
