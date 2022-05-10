ed5c942c212e55a0f370f308fe24fddbb1575b89
kie_finnomena <kie@finnomena.com>
2022-05-10 13:20:24 +0700

    fix: handle error when writing cache file

--- src/utils/makeMvnArtifactJson.js
+++ src/utils/makeMvnArtifactJson.js
@@ -16,19 +16,30 @@ async function makeMvnArtifactJson({ groupId, artifactId, version }) {
     const url = makeArtifactPomUrl({ groupId, artifactId, version })
 
     const response = await fetch(url)
-    !fs.existsSync(".tmp") && fs.mkdirSync(".tmp")
+    const pomContent = await response.text();
+    try {
+        !fs.existsSync(".tmp") && fs.mkdirSync(".tmp")
 
-    const pomPath = `.tmp/tmp-${groupId}:${artifactId}-pom.xml`
-
-    await fs.writeFile(pomPath, await response.text())
-
-    const { dependencies: parsedPom } = await parsePom({ filePath: pomPath })
-
-    Object.keys(parsedPom).forEach(
-        key => (parsedPom[key] = parseMvnVersion(parsedPom[key]))
-    )
-
-    fs.removeSync(".tmp")
+        const pomPath = `.tmp/tmp-${groupId}:${artifactId}-pom.xml`
+    
+        await fs.writeFile(pomPath, pomContent)
+    
+        const { dependencies: parsedPom } = await parsePom({ filePath: pomPath })
+    
+        Object.keys(parsedPom).forEach(
+            key => (parsedPom[key] = parseMvnVersion(parsedPom[key]))
+        )
+    
+        fs.removeSync(".tmp")
+    } catch (e) {
+        console.error("error occur on makeMvnArtifactJson, skip writing file. error:", e)
+        
+        const { dependencies: parsedPom } = await parsePom({xmlContent: pomContent})
+    
+        Object.keys(parsedPom).forEach(
+            key => (parsedPom[key] = parseMvnVersion(parsedPom[key]))
+        )
+    }
 
     return parsedPom
 
