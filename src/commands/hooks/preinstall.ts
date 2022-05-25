0db28584a5f3293a5921f6edd239d7192707c651
Narongrit Unwerawattana <narongrit.kie@gmail.com>
2022-05-25 21:54:39 +0200

    add global flag for install

--- src/commands/hooks/preinstall.ts
+++ src/commands/hooks/preinstall.ts
@@ -1,4 +1,4 @@
-import { Command } from '@oclif/core'
+import { Command, Flags } from '@oclif/core'
 import { ERR_NOT_JPM } from '../../errors'
 import chalk from 'chalk'
 export default class HooksPreinstall extends Command {
@@ -11,8 +11,18 @@ export default class HooksPreinstall extends Command {
     'jpm hooks preinstall'
   ]
 
+  static override flags = {
+    allowGlobal: Flags.boolean({ description: 'allow npm install -g jolie-package' })
+  }
+
   public async run(): Promise<void> {
+    const { flags } = await this.parse(HooksPreinstall)
+    if (flags.allowGlobal && process.env['npm_config_global'] === 'true') {
+      return
+    }
+
     if (process.env['npm_execpath'] && process.env['npm_execpath'].indexOf('jpm') === -1) {
+
       throw ERR_NOT_JPM
     }
   }
